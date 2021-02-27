5764eb97699cdd41b519b8cc7a90bdcebf214fbd
lknlookin <lknlookin@gmail.com>
2021-02-28 02:02:08 +0700

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
-import { CountingComponent } from './counting/counting.component';
-import { Lab4Component } from './lab/lab4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CountingComponent,
-    Lab4Component
+    HomeComponent
   ],
   imports: [
     BrowserModule,
