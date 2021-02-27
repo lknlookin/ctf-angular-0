d80c6cc61bcbe7073043d81c9f4e38b9147b44f4
lknlookin <lknlookin@gmail.com>
2021-02-28 04:13:11 +0700

    lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { CountingComponent } from './counting/counting.component';
-import { Lab4Component } from './lab4/lab4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CountingComponent,
-    Lab4Component
+    CountingComponent
   ],
   imports: [
     BrowserModule,
